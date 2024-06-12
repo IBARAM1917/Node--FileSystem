edae5d1b179240c68831b038fb15fff3744f6b7c
IBARAM1917 <abiraminagaraj22@gmail.com>
2024-06-12 18:40:23 +0530

    updated

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 import express from "express";
+import fs from 'node:fs';
+import { format } from "date-fns";
+import path from "path";
+import { fileURLToPath } from "url";
 //importing space
 
 
 //declaration /intailization
 const app=express();
 const PORT=4000;
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+const directorypath=path.join(__dirname,'Timestamp');
+console.log(directorypath);
 
 //midleware
 app.use(express.json())
 
 //routes
-app.get('/',(req,res)=>{
-//res.status(200).json({message:'hii all wellcome'})
-res.status(200).send(`<div style="background-color:Aqua;color:black"><h1>welcome to my first App</h1></div>`)
-})
-app.get("/data",(req,res)=>{
-    res.status(200).send(`<div style="background-color:Aqua;color:black"><h1>welcome to my Home</h1></div>`)  
+let filelocation;
+app.get(`/create`,(req,res)=>{
+    let today=format(new Date(),`dd-MM-yyyy HH-mm-ss`);
+    console.log(today);
+    filelocation=`Timestamp/${today}.txt`;
+    fs.writeFileSync(filelocation,`${today}`,'utf-8');
+    res.status(200).send(`${today}.txt Create Timestamp Folder`);
 })
 
+app.get(`/read`,(req,res)=>{
+    let txtfile=[];
+    fs.readdir(directorypath,(err,files)=>{
+        files.forEach((item)=>{
+            txtfile.push(item);
+        });
+        res.status(200).send(txtfile.join('<br/>'));
+    });
+})
 //running port
 app.listen(PORT,()=>{
     console.log(`App is listening on the port  ${PORT}`);
